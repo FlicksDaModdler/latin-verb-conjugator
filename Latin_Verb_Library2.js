1aeb51d91e2dab5f570bd2b50f8ffe1092b49710
FlicksDaModdler <105262569+FlicksDaModdler@users.noreply.github.com>
2022-05-10 00:10:50 -0400

    file
    fh

---
+++ Latin_Verb_Library2.js
@@ -0,0 +1,6 @@
+function latinVerbs1stConjugation() {
+    var abacino = ['abacinō', 'abacināre', ' abacināvī', 'abacinātus'];
+    var abaestumo = ['abaestumō', 'abaestumāre', 'abaestumāvī' , 'abaestumātus'];
+    var abaestuo = ['abaestuō' , 'abaestuāre' , 'abaestuāvī' , 'abaestuātus'];
+}
+export {latinVerbs1stConjugation};
